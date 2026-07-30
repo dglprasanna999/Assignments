b1d424a69fe6c47e16158b30ce55fc9552baf6f9
Prasanna Kumar <dglprasanna999@gmail.com>
2026-07-30 10:31:02 +0530

    Assignment demo

---
+++ javascript-training/hello-world.js
@@ -0,0 +1,15 @@
+console.log("Hello world javascript");
+
+console.log("open account");
+launchBrowser()
+console.log("close application");
+
+function launchBrowser(){
+    console.log("login");
+    let accountBalance = 10000;
+    
+    console.log("logout");
+
+    return accountBalance;
+
+}
